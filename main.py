7f5dbe49cf38bc8758d82cad6ad60d62afaa338d
Tomáš Turecký <t_turecky@utb.cz>
2025-04-28 13:41:22 +0200

    Backpack problem solution now correctly solved with local search. Able to complete the previously bruteforced work of 1 hour in under 1 tenth of a second.

--- main.py
+++ main.py
@@ -20,8 +20,8 @@ def generate_all_permutations(nclasses,nitems):
 def bruteforce_best_permutation(objects, permutations, item_count):
     start_time = datetime.datetime.now()
     best_permutation_objects = []
-    best_price = 99999999999
-    best_size = 99999999999
+    best_price = 0
+    best_size = 0
     max_size = int(len(objects)/item_count*20)
     for j,permutation in enumerate(permutations):
         permutation_size = 0
@@ -33,7 +33,7 @@ def bruteforce_best_permutation(objects, permutations, item_count):
             permutation_price += current_object[3]
             permutation_objects.append(current_object)
         if permutation_size <= max_size:
-            if permutation_price <= best_price:
+            if permutation_price >= best_price:
                 best_price = permutation_price
                 best_size = permutation_size
                 best_permutation_objects = permutation_objects
@@ -69,17 +69,55 @@ def generate_neighbor(center_point,item_count):
             break
     return neighbor
 
-def localsearch_best_permutation(objects, permutations, item_count, max_iterations):
-    print('localsearch')
+def localsearch_best_permutation(objects, item_count, max_iterations, population):
+    start_time = datetime.datetime.now()
+    center_point = [random.randint(0,item_count-1) for i in range(int(len(objects)/item_count))]
+    center_result = test_function(center_point,objects,item_count)
+    convergence_results = []
+    max_size = int((len(objects) / item_count) * 20)
+    best_result = (0, 999999999)
+    best_point = 0
+    for i in range(max_iterations):
+        best_result = (0,999999999)
+        best_point = 0
+        for x in range(population):
+            coords = generate_neighbor(center_point, item_count)
+            local_test = test_function(coords, objects, item_count)
+            current_price = local_test[0]
+            current_size = local_test[1]
+            if best_result[0] == 0:
+                best_result = center_result
+                best_point = center_point
+            if current_size <= max_size:
+                if current_price >= best_result[0] or best_result[1] > max_size:
+                    best_result = local_test
+                    best_point = coords
+        center_point = best_point
+        center_result = best_result
+        if len(convergence_results) == 0:
+            convergence_results.append(best_result)
+        elif convergence_results[len(convergence_results) - 1] > best_result:
+            convergence_results.append(best_result)
+        else:
+            convergence_results.append(convergence_results[len(convergence_results) - 1])
+    end_time = datetime.datetime.now()
+    duration = (end_time - start_time).total_seconds()
+    print(f'Calculation duration: {duration} s')
+    best_permutation_objects = [[i+1,item+1,objects[i * item_count + item][2],objects[i * item_count + item][3]] for i,item in enumerate(best_point)]
+    row_labels = ['Trida', 'Id', 'Objem', 'Cena']
+    plt.table(cellText=best_permutation_objects, colLabels=row_labels, loc='center')
+    plt.axis('off')
+    plt.title(f'Nejlepší kombinace předmětů\nSumy - Objem: {best_result[1]} Cena: {best_result[0]}')
+    plt.show()
+    return convergence_results
 
 
 if __name__ == '__main__':
-    class_count = 5
+    class_count = 19
     item_count = 3
-    # generated_objects = generate_objects(class_count,item_count)
+    generated_objects = generate_objects(class_count,item_count)
+
     # possible_permutations = generate_all_permutations(class_count,item_count)
     # bruteforce_best_permutation(generated_objects, possible_permutations,item_count)
-    # print(test_function((0,1,0,2,2),generated_objects,item_count))
 
-    test_center = [1,2,0]
-    print(generate_neighbor(test_center,3))
+    localsearch_best_permutation(generated_objects,item_count,1000,5)
